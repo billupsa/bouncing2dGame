43930e30ec4bd7d8818ee65b4a7bef12021d730b
Alexander Billups <60077749+billupsa@users.noreply.github.com>
2021-10-31 17:03:18 -0400

    Add files via upload

---
+++ MainTeleporter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTeleporter : MonoBehaviour
+{
+    public Transform starter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Once the player passes the plane for this onTriggerEnter, then the player is teleported to the starting position of the main map
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ManMovement.man.transform.position = starter.transform.position;
+    }
+}
